e788cbb8423b73410433d601dbd7dbabd5e65d25
신주훈 <junefinger@gmail.com>
2024-12-27 23:19:57 +0900

    [작업] cors 추가

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// `/images` 요청이 들어오면 전체 이미지 배열 반환
-app.get('/images', (req, res) => {
-  // `db.json` 파일 읽기
-  fs.readFile('./db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading db.json:', err);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+// `/images` 경로로 정적 파일 제공
+//app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
-    try {
-      const db = JSON.parse(data);
-      const images = db.images || []; // images 배열 추출
-      res.json(images); // JSON 응답
-    } catch (parseError) {
-      console.error('Error parsing db.json:', parseError);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+// CORS 설정
+app.use(cors());
+
+app.get('/images', (req, res) => {
+    fs.readFile('./db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading db.json:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        try {
+            const db = JSON.parse(data);
+            const images = db.images || [];
+            res.header("Content-Type", "application/json"); // 응답 헤더를 JSON으로 설정
+            res.send(JSON.stringify(images, null, 2)); // JSON을 예쁘게 포맷하여 전송
+        } catch (parseError) {
+            console.error('Error parsing db.json:', parseError);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
 });
+
 
 // 서버 시작
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
